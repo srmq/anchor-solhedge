d1733b729481247f6897a4f689e3ac13837510d1
Sergio Queiroz <srmq@srmq.org>
2023-06-19 19:49:39 -0300

    workinprogress taker_settle_put_option

--- programs/anchor-solhedge/src/lib.rs
+++ programs/anchor-solhedge/src/lib.rs
@@ -195,6 +195,11 @@ pub mod anchor_solhedge {
         Ok(result)
     }
 
+    pub fn taker_settle_put_option(ctx: Context<TakerSettlePutOption>) -> Result<()> {
+        //FIXME TODO
+        Ok(())
+    }
+
     pub fn taker_adjust_funding_put_option_vault(ctx: Context<TakerAdjustFundingPutOptionVault>,
         new_funding: u64
     ) -> Result<u64> {
@@ -790,6 +795,92 @@ pub mod anchor_solhedge {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct TakerSettlePutOption<'info> {
+    #[account(
+        constraint = vault_factory_info.strike > 0,
+        constraint = vault_factory_info.is_initialized == true,
+        constraint = vault_factory_info.matured == true,
+        constraint = vault_factory_info.settled_price > 0,
+        constraint = vault_factory_info.base_asset == base_asset_mint.key(),
+        constraint = vault_factory_info.quote_asset == quote_asset_mint.key(),
+
+    )]
+    pub vault_factory_info: Account<'info, PutOptionVaultFactoryInfo>,
+
+    #[account(
+        mut,
+        seeds=[
+            "PutOptionVaultInfo".as_bytes().as_ref(), 
+            vault_factory_info.key().as_ref(),
+            vault_info.ord.to_le_bytes().as_ref()
+        ], bump,
+        constraint = vault_info.factory_vault == vault_factory_info.key(),
+    )]
+    pub vault_info: Account<'info, PutOptionVaultInfo>,
+
+    #[account(
+        mut,
+        seeds=[
+            "PutOptionTakerInfo".as_bytes().as_ref(),
+            vault_factory_info.key().as_ref(),
+            vault_info.ord.to_le_bytes().as_ref(), 
+            initializer.key().as_ref()
+        ],
+        bump,
+        constraint = !put_option_taker_info.is_settled
+    )]
+    pub put_option_taker_info: Account<'info, PutOptionTakerInfo>,
+
+    // mint for the base_asset
+    pub base_asset_mint: Account<'info, Mint>,
+
+    // mint for the quote asset
+    pub quote_asset_mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        constraint = vault_base_asset_treasury.mint == base_asset_mint.key(), // Base asset mint
+        constraint = vault_base_asset_treasury.owner.key() == vault_info.key() // Authority set to vault PDA
+    )]
+    pub vault_base_asset_treasury: Box<Account<'info, TokenAccount>>,
+
+    #[account(
+        mut,
+        constraint = vault_quote_asset_treasury.mint == quote_asset_mint.key(), // quote asset mint
+        constraint = vault_quote_asset_treasury.owner.key() == vault_info.key() // Authority set to vault PDA
+    )]
+    pub vault_quote_asset_treasury: Box<Account<'info, TokenAccount>>,
+
+
+    // if put option is not exercised, taker will get her base tokens back at this account
+    #[account(
+        mut,
+        constraint = taker_base_asset_account.owner.key() == initializer.key(),
+        constraint = taker_base_asset_account.mint == base_asset_mint.key()
+    )]
+    pub taker_base_asset_account: Box<Account<'info, TokenAccount>>,
+
+    // if put option is not exercised, taker will get her base tokens back at this account
+    #[account(
+        mut,
+        constraint = taker_quote_asset_account.owner.key() == initializer.key(),
+        constraint = taker_quote_asset_account.mint == quote_asset_mint.key()
+    )]
+    pub taker_quote_asset_account: Box<Account<'info, TokenAccount>>,
+
+
+    // Check if initializer is signer, mut is required to reduce lamports (fees)
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    
+    // System Program requred for deduction of lamports (fees)
+    pub system_program: Program<'info, System>,
+    // Token Program required to call transfer instruction
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>
+}
 
 #[derive(Accounts)]
 #[instruction(
